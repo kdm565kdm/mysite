da9e75ae9a05a4838a63159aee48db70147ce6e3
ox <1476045903@qq.com>
2018-01-08 19:51:32 +0900

    gui

--- weather/views.py
+++ weather/views.py
@@ -4,60 +4,99 @@ from django.shortcuts import HttpResponse
 import json
 from urllib import request
 from lxml import etree
-#import types
-
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '  
-                        'Chrome/63.0.3239.108 Safari/537.36'}
 
 
-weather_dic = {}
-infos = []
 
-def get_weather(url):
-    req = request.Request(url=url,headers=headers)
-    res = request.urlopen(req)
-    html = res.read()
-    html_code = html.decode('utf-8')
-    selector = etree.HTML(html_code)
-    days = selector.xpath('//*[@id="7d"]/ul/li')
-    for day in days:
-        date = day.xpath('h1/text()')[0].strip()
-        rain = day.xpath('p[1]/text()')[0]
-        try:
-            temperature_a = day.xpath('p[2]/span/text()')[0]
-        except:
-            temperature_a = '0℃'
-        temperature_c = day.xpath('p[2]/i/text()')[0]
-        temperature = temperature_a+'~'+temperature_c
-        wind = day.xpath('p[3]/em/span[1]/@title')[0]
-        wind_degree = day.xpath('p[3]/i/text()')[0]
-        infos = []
-        infos.append(date)
-        infos.append(rain)
-        infos.append(temperature)
-        #temp = {wind:wind_degree,}
-        infos.append(wind)
-        infos.append(wind_degree)
-        weather_dic[date] = infos
 
-def weather(city):
-    fp = open('E:/python_spider/area_code.json','r')
-    json_data = fp.read()
-    content = json.loads(json_data)
-    #word = input('请输入地点：')
-    word = city
-    if isinstance(word,str) == True:
-        area = content[word]
-        print(area)
-        url = 'http://www.weather.com.cn/weather/{}.shtml'.format(area)
-        print(url)
-        get_weather(url)
-    fp.close()
+
+
+
+class weather:
+    def __init__(self, url, headers):
+        self.url = url
+        self.headers = headers
+        self.weather_dic = []
+        self.infos = []
+    def get_weather(self):
+        req = request.Request(url=self.url,headers=self.headers)
+        res = request.urlopen(req)
+        html = res.read()
+        html_code = html.decode('utf-8')
+        selector = etree.HTML(html_code)
+        days = selector.xpath('//*[@id="7d"]/ul/li')
+        for day in days:
+            date = day.xpath('h1/text()')[0].strip()
+            rain = day.xpath('p[1]/text()')[0]
+            if rain.find('晴') != -1:
+                icon = '<img src="/static/images/weather_icons/sun.png" class="img-responsive">'
+            elif rain.find('云') != -1:
+                icon = '<img src="/static/images/weather_icons/has_cloud.png" class="img-responsive">'
+            elif rain.find('阴') != -1:
+                icon = '<img src="/static/images/weather_icons/shadow.png" class="img-responsive">'
+            elif rain.find('雨') != -1:
+                if rain.find('小雨') != -1:
+                    icon = '<img src="/static/images/weather_icons/small_rain.png" class="img-responsive">'
+                elif rain.find('中雨') != -1:
+                    icon = '<img src="/static/images/weather_icons/middle_rain.png" class="img-responsive">'
+                elif rain.find('大雨') != -1:
+                    icon = '<img src="/static/images/weather_icons/big_rain.png" class="img-responsive">'
+                elif rain.find('暴雨') != -1:
+                    icon = '<img src="/static/images/weather_icons/large_rain.png" class="img-responsive">'
+                elif rain.find('雷') != -1:
+                    icon = '<img src="/static/images/weather_icons/rain_lighting.png" class="img-responsive">'
+                else:
+                    icon = '<img src="/static/images/weather_icons/rain_lighting.png" class="img-responsive">'
+            elif rain.find('雪') != -1:
+                icon = '<img src="/static/images/weather_icons/snow.png" class="img-responsive">'
+            else:
+                icon = '<img src="/static/images/weather_icons/sun.png" class="img-responsive">'
+            try:
+                temperature_a = day.xpath('p[2]/span/text()')[0]
+            except:
+                temperature_a = '0℃'
+            temperature_c = day.xpath('p[2]/i/text()')[0]
+            temperature = temperature_a+'~'+temperature_c
+            wind = day.xpath('p[3]/em/span[1]/@title')[0]
+            wind_degree = day.xpath('p[3]/i/text()')[0]
+            self.infos = []
+            self.infos.append(date)
+            self.infos.append(icon)
+            self.infos.append(rain)
+            self.infos.append(temperature)
+            self.infos.append(wind)
+            self.infos.append(wind_degree)
+            self.weather_dic.append(self.infos)
+        return self.weather_dic
+
+class infos_io():
+    def __init__(self, fp, city):
+        self.fp = fp
+        self.city = city
+    def parse_json(self):
+        json_data = self.fp.read()
+        content = json.loads(json_data)
+        if isinstance(self.city,str) == True:
+            area = content[self.city]
+            print(area)
+            return area
+
 
 def index(request):
-    global weather_dic
     if request.method == "POST":
-        weather_dic = {}
+        fp = open('E:/python_spider/area_code.json','r')
+
+        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '  
+                        'Chrome/63.0.3239.108 Safari/537.36'}
+        #weather.weather_dic = {}
         city = request.POST.get("city",None)
-        weather(city)
-    return render(request,"index.html",{"data":weather_dic.items()})
+        #infos_io = infos_io(fp, city)
+        try:
+            area_code = infos_io(fp, city).parse_json()
+        except:
+            return render(request, "error.html", )
+        url = 'http://www.weather.com.cn/weather/{}.shtml'.format(area_code)
+        weather_dic = weather(url,headers).get_weather()
+        print(weather_dic[0])
+        fp.close()
+        return render(request, "index.html", {"data": weather_dic,"city":city})
+    return render(request,"index.html",)
