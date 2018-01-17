3a56001a4d6cb325e926676910904efc00841aba
kdm565 <1476045903@qq.com>
2018-01-17 18:04:22 +0800

    fix json bug

--- spider_flower.py
+++ spider_flower.py
@@ -8,8 +8,9 @@ class beauty:
         req = request.Request(url=urls, headers=headers)
         res = request.urlopen(req)
         html = res.read()
+        html_code = html.decode('utf-8')
         self.srcs = []
-        self.json_data = json.loads(html)
+        self.json_data = json.loads(html_code)
     def get_src(self):
         results =  self.json_data["results"]
         for result in results:
